a6ee7fbf70d82390a866986bebcb93bb4526e7fc
Jirka Kremser <jkremser@redhat.com>
2017-05-21 12:59:58 +0200

    adding more hates

--- generator.js
+++ generator.js
@@ -1,35 +1,21 @@
 (function () {
-String.prototype.formatUnicorn = String.prototype.formatUnicorn ||
-function () {
-  "use strict";
-  var str = this.toString();
-  if (arguments.length) {
-    var t = typeof arguments[0];
-    var key;
-    var args = ("string" === t || "number" === t) ?
-    Array.prototype.slice.call(arguments)
-    : arguments[0];
 
-    for (key in args) {
-      str = str.replace(new RegExp("\\{" + key + "\\}", "gi"), args[key]);
-    }
-  }
-
-  return str;
-};
-
+// some random hates found on the internets
 var other = ['Prece  nam nebudou rikat jestli mame mit pravo saria , pivo a klobasku bych jim povine dal vsem snizt. ',
   ' to by se za komunistu stat nemohlo, zlaty komunisti!!!!',
   ' republiku si  rozvracen nenechame ',
   ' a media/ceska televize mlci',
-  'vyresilo byse to kdy by byl Ortel na hrad',
+  ' ja nejsem rasista..',
+  ' Muzete laskave vysvetlit proc panskou modu prezentujete na "cernochovi"??',
+  ' Chci se zeptat jestli tento cerny model je opravdu nutny ve vasem letaku. je?',
+  ' a vyresilo byse to kdy by byl Ortel na hrad ',
   ' znama  z nemecka rikala, ze  vubec nepracujou a  jenom porad berou davky ',
-  'pro alahovu hlavu uriznou ti hlavu ',
-  'a nejhorsi jsou ti uzitecni idioti co by nejraci pozvali vsechny ty muslimske teroristy.kdybysme jeli mi k nim,tak nas odstreli hned vsechny v mori!!',
+  ' nojono pro alahovu hlavu uriznou ti hlavu ',
+  ' a nejhorsi jsou ti uzitecni idioti co by nejraci pozvali vsechny ty muslimske teroristy.kdybysme jeli mi k nim,tak nas odstreli hned vsechny v mori!!',
   'Cina a Rusko jsou aspon pro tuto zemy prinos ,coz se o tech agentech CIA rict neda !!!',
   '1000ice migrantu nam chce brat nase hodnoty,musime chranit nase hranice!!!  ',
-  'Stejne tak jako USA  nebyly nikdy na mesyci ,tak media mlci o te velke pravde o imigrantech',
-  ' , je to vsechno naplanovane kvuli new world order.',
+  'Stejne tak jako USA  nebyly nikdy na mesyci ,tak media mlci o te velke pravde o imigrantech ',
+  ' je to vsechno naplanovane kvuli new world order(NWO).',
   'Otevrete uz konecne oci, vy tupe ovce! !',
   ' jinak ja jsem hrdy na nase hodnoty, co si dokazal ty ty havlomrde jeden,kurvo ?  ',
   ' mymochodem Ortel zpiva jenom pravdu a toho slavika si priste snad odnese a toho cigana by meli zavrit nekam do zoo .',
@@ -38,22 +24,37 @@ var other = ['Prece  nam nebudou rikat jestli mame mit pravo saria , pivo a klob
   'Tisice migrantu se sem vali a v CT zase nic!!',
   ' jediny okamura to tam rika tak jak to doopravdy je...',
   ' aspon ze ti nasi hokejisti neco hrajou,my jsi dyktovat z EU NEDAME !!',
-  'pan   prezident  je dobry clovek,proto media lzou  ',
+  '. Pan   prezident  je dobry clovek,proto media lzou  ',
   ' uz to psali i v parlamentnich listech ',
   ' lidi probudte se uz konecne kam az to nechame zajit?!',
   ' svrhneme tuto prohnilou vladu a nastolme vladu lidu!!!!!',
-  ' tohle uz je snad horsi nez ten negr v bile',
+  ' tohle uz je snad horsi nez ten negr  v bile ',
   ' k lopatam je nahnat vsechny',
-  ' sulini jedni',
+  ' kdyby ty cikorky pracovaly,takby tohle nebylo!',
+  ' ..sulini jedni ',
   ' pivo bych jim vsem povine naordinoval ',
   ' zapalte ohne',
+  ' a co ma jako znamenat ten cernoch na letaku???',
+  ' a bezte s multi-kulti do prdele!!',
+  ' cechy cechum!!!!',
+  'Odstrante ty multikulty picoviny!',
+  ' protoze zatim jeste plati ze mi tady jsme doma a ne ti paraziti...',
+  ' je to rizena genocida bile rasy ',
+  ' nejsem rasista,evropa je bila afrika je cerna a asie je zluta ',
+  ' a vubec proc se v nasich ceskych letacich pro ceskou republiku neustale objevuji modelove NEGROIDNIHO typu??!',
+  ' Tak s timhle jdete vite kam..... stejne jako s chemtrails',
+  ' prece nejsem xenofob ,kdyz si to myslm ',
+  ' nejsem islamofob , ale ',
+  ' jako ja nejsem rasista ,ale muslimaky bych postrilel hned na hranici!',
+  ' protoze jedinym resenim migracni krize je kulomet...',
   ' jenze Rusko aspon nedela ty valky kvuli te rope',
-  ' a nase duchodce musime SBIRAT VICKA???? ',
-  ',ale DVTV je prozapadni bulvarni zumpa takze asitak..',
-  'holt v tehle vlasti uz zacalo pusobit pravo saria kam to az zajde.....',
+  ' a na nase duchodce musime SBIRAT VICKA???? ',
+  ' ale DVTV je prozapadni bulvarni zumpa takze asitak..',
+  ' vsichni pravdolaskari bysi mneli vspomenout na humanytarni bombardovani havlem!!',
+  ' holt v tehle vlasti uz zacalo pusobit pravo saria kam to az zajde.....',
   'Hehehe,natrtkalisi ,at se staraji.'];
 
-var goodPplSentences = [
+var goodGuysSentences = [
   [1, ' politice sice moc nerozumim ale ten {0} je dobry, ten by tam aspon udelal poradek .'],
   [1, ' {0} chce aby jsme mohly branit nasi zemy.'],
   [1, ' {0} to misli uprimne.'],
@@ -64,9 +65,9 @@ var goodPplSentences = [
   [1, ' {0} je tam aspon srovna ty vlastizradce  !!!  ']
 ];
 
-var badPplSentences = [
-  [2, ' z mojich dani nebudu platit tyhle neprizpusobive ,, nebudte ovce...., za vsechno muzou {0} a {1} .'],
-  [2, 'Exystuje tajny plan , ktery vytvorili {0} a {1} a ted se to vsechno deje otevrete uz konecne oci  '],
+var badGuysSentences = [
+  [2, ' z mojich dani nebudu platit tyhle "neprizpusobive" ,, nebudte ovce...., za vsechno muzou {0} a {1} .'],
+  [2, 'Exystuje tajny plan , ktery vytvorili {0} a {1} a ted se to vsechno deje otevrete uz konecne oci '],
   [2, '{0} a {1} stejne porad jenom okradaji stat a slusne pracujici lidi.'],
   [1, '{0} do plynu!!!!'],
   [1, ' ,proto ze tohle jsou veci bez kterych by tato republika fungovala: {0} a {1} '],
@@ -80,32 +81,63 @@ var badPplSentences = [
   [1, 'Jinak vcera jsem se dival na oblohu a bila tam velka davka chemtrails vetsi nez obvikle muzou za to {1} a Kalousek!!!!!!']
 ];
 
-var goodPpl = ['pan Babis', 'Babis', 'pan mluvci Ovcacek', 'Zeman', 'pan prezident', 'Okamura', 'Konvicka',
-   'pan prezident Putin', 'Putin', 'Ortel', 'prezident Trump', 'Fico', 'Adam B. Bartos', 'SPD', 'KSCM'];
+var goodGuys = ['pan Babis', 'Babis', 'pan mluvci Ovcacek', 'Zeman', 'pan prezident', 'Okamura', 'pan Konvicka',
+   'pan prezident Putin', 'Putin', 'Ortel', 'prezident Trump', 'Adam B. Bartos', 'SPD', 'KSCM'];
 
-var badPpl = ['Sobotka', 'vlastizradce Sobotka', 'slunickari', 'prohavlovska pakaz', 'kolaboranti', 'muslimaci',
+var badGuys = ['Sobotka', 'vlastizradce Sobotka', 'slunickari', 'prohavlovska pakaz', 'kolaboranti', 'muslimaci',
  'Kalousek', 'fasisti na ukrajine', 'havloidi', 'birokrati z EU', 'Diensbier', 'Horacek', 'Obama',
  'makrela', 'zmrd Kalousek', 'Merkelova', 'nadnarodni elity', 'prazska kavarna', 'novinarska zumpa',
  'iluminati', 'USA ambasada', 'CIA', 'korporace', 'diktat z EU', 'islam', 'cigani', 'Moravec', 'Kocab'];
 
+// monkey patch the formatting function into strings
+ String.prototype.formatUnicorn = String.prototype.formatUnicorn ||
+ function () {
+   "use strict";
+   var str = this.toString();
+   if (arguments.length) {
+     var t = typeof arguments[0];
+     var key;
+     var args = ("string" === t || "number" === t) ?
+     Array.prototype.slice.call(arguments)
+     : arguments[0];
+
+     for (key in args) {
+       str = str.replace(new RegExp("\\{" + key + "\\}", "gi"), args[key]);
+     }
+   }
+
+   return str;
+ };
+
+// randomly use capslock and add some bangs
 function buranize(sentence, howMuchCapslock, howMuchTypos) {
   var splits = sentence.split(/([\s,.!?]+)/g);
-  return splits.map(function(chunk) {
-    if (Math.random() < howMuchCapslock) {
+  var result = splits.map(function (chunk) {
+    if (Math.random() < howMuchCapslock && (chunk.length > 2 || howMuchCapslock > 0.6)) {
       return chunk.toUpperCase();
     } else {
       return chunk;
     }
   }).join('');
-  // todo typos
+  if (howMuchCapslock > 0.4) {
+    result += '!!!';
+  }
+  if (howMuchCapslock > 0.6) {
+    result += '!!!!!';
+  }
+  if (howMuchCapslock > 0.8) {
+    result += '!!!!!!!!!!!!!!!?!!!!!!';
+  }
+  return result;
+  // todo add random typos
 }
 
+// shuffle the array (w/ plain JS)
 function shuffle(array) {
   var currentIndex = array.length, temporaryValue, randomIndex;
 
   // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-
+  while (currentIndex !== 0) {
     // Pick a remaining element...
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex -= 1;
@@ -121,55 +153,51 @@ function shuffle(array) {
 
 function getRandomSentence() {
   var r = Math.random();
-  if (r < 0.17) {
-    //goodPplSentences
-    var item = goodPplSentences.splice(Math.floor(Math.random()*goodPplSentences.length), 1);
-    var numberOfPeople = item[0][0];
+  if (r < 0.12) {
+    //goodGuysSentences
+    var item = goodGuysSentences.splice(Math.floor(Math.random() * goodGuysSentences.length), 1);
+    // var numberOfPeople = item[0][0];
     var sentenceTemplate = item[0][1];
-    var goodPplShuffled = shuffle(goodPpl);
-    return sentenceTemplate.formatUnicorn(goodPplShuffled);
-  } else if (r < 0.35) {
-    //badPplSentences
-    var item = badPplSentences.splice(Math.floor(Math.random()*badPplSentences.length), 1);
-    var numberOfPeople = item[0][0];
+    return sentenceTemplate.formatUnicorn(shuffle(goodGuys));
+  } else if (r < 0.25) {
+    //badGuysSentences
+    var item = badGuysSentences.splice(Math.floor(Math.random() * badGuysSentences.length), 1);
+    // var numberOfPeople = item[0][0];
     var sentenceTemplate = item[0][1];
-    var badPplShuffled = shuffle(badPpl);
-    return sentenceTemplate.formatUnicorn(badPplShuffled);
+    return sentenceTemplate.formatUnicorn(shuffle(badGuys));
   } else {
     //other
-    return other.splice(Math.floor(Math.random()*other.length), 1);
+    return other.splice(Math.floor(Math.random() * other.length), 1);
   }
 }
 
 function getPost(length, capslock) {
   // length 2 - 5
   length = length || Math.random() * 3 + 2;
-  capslock = capslock || 0.05
-  var goodPplSentencesBak = goodPplSentences.slice(0);
-  var badPplSentencesBak = badPplSentences.slice(0);
+  capslock = capslock || 0.05;
+  var goodGuysSentencesBak = goodGuysSentences.slice(0);
+  var badGuysSentencesBak = badGuysSentences.slice(0);
   var otherBak = other.slice(0);
 
   var post = '';
-  for (i = 0; i < length; i++) {
+  for (var i = 0; i < length; i++) {
     post += getRandomSentence();
   }
 
-  goodPplSentences = goodPplSentencesBak;
-  badPplSentences = badPplSentencesBak;
+  goodGuysSentences = goodGuysSentencesBak;
+  badGuysSentences = badGuysSentencesBak;
   other = otherBak;
   return buranize(post, capslock);
 }
 
 function generatePost() {
-  var rage = parseInt(document.getElementById('rageFactor').value)/12;
+  var rage = parseInt(document.getElementById('rageFactor').value) / 12;
   var post = getPost(undefined, rage);
   document.getElementById('post').innerHTML = '';
   document.getElementById('post').insertAdjacentHTML('afterbegin', post);
 }
 
-window.getPost = getPost;
-window.buranize = buranize;
-window.getRandomSentence = getRandomSentence;
+// hacky export, but let's keep it simple
 window.generatePost = generatePost;
 
 return {
