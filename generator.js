f05c65587a1a8a60b5823b5f82caeade467185e1
Jirka Kremser <jkremser@redhat.com>
2017-05-21 15:00:22 +0200

    rage factor improvements

--- generator.js
+++ generator.js
@@ -1,93 +1,100 @@
 (function () {
 
 // some random hates found on the internets
-var other = ['Prece  nam nebudou rikat jestli mame mit pravo saria , pivo a klobasku bych jim povine dal vsem snizt. ',
-  ' to by se za komunistu stat nemohlo, zlaty komunisti!!!!',
-  ' republiku si  rozvracen nenechame ',
-  ' a media/ceska televize mlci',
+var other = ['Přece  nám nebudou říkat jestli máme mít právo šaria , pivo a klobásku bych jim povině dal všem snízt. ',
+  ' to by se za komunistů stát nemohlo, zlatý komunisti!!!!',
+  ' republiku si  rozvracet nenecháme ',
+  ' a média a česká televize mlčí',
   ' ja nejsem rasista..',
-  ' Muzete laskave vysvetlit proc panskou modu prezentujete na "cernochovi"??',
-  ' Chci se zeptat jestli tento cerny model je opravdu nutny ve vasem letaku. je?',
-  ' a vyresilo byse to kdy by byl Ortel na hrad ',
-  ' znama  z nemecka rikala, ze  vubec nepracujou a  jenom porad berou davky ',
-  ' nojono pro alahovu hlavu uriznou ti hlavu ',
-  ' a nejhorsi jsou ti uzitecni idioti co by nejraci pozvali vsechny ty muslimske teroristy.kdybysme jeli mi k nim,tak nas odstreli hned vsechny v mori!!',
-  'Cina a Rusko jsou aspon pro tuto zemy prinos ,coz se o tech agentech CIA rict neda !!!',
-  '1000ice migrantu nam chce brat nase hodnoty,musime chranit nase hranice!!!  ',
-  'Stejne tak jako USA  nebyly nikdy na mesyci ,tak media mlci o te velke pravde o imigrantech ',
-  ' je to vsechno naplanovane kvuli new world order(NWO).',
-  'Otevrete uz konecne oci, vy tupe ovce! !',
-  ' jinak ja jsem hrdy na nase hodnoty, co si dokazal ty ty havlomrde jeden,kurvo ?  ',
-  ' mymochodem Ortel zpiva jenom pravdu a toho slavika si priste snad odnese a toho cigana by meli zavrit nekam do zoo .',
-  'Snad bude MENE PRIVANDROVALCU JESTLI MI ROZUMITE??!!!',
-  ' a ikdyz to neni pravda tak by MOHLA BYT!!!',
-  'Tisice migrantu se sem vali a v CT zase nic!!',
-  ' jediny okamura to tam rika tak jak to doopravdy je...',
-  ' aspon ze ti nasi hokejisti neco hrajou,my jsi dyktovat z EU NEDAME !!',
-  '. Pan   prezident  je dobry clovek,proto media lzou  ',
-  ' uz to psali i v parlamentnich listech ',
-  ' lidi probudte se uz konecne kam az to nechame zajit?!',
-  ' svrhneme tuto prohnilou vladu a nastolme vladu lidu!!!!!',
-  ' tohle uz je snad horsi nez ten negr  v bile ',
-  ' k lopatam je nahnat vsechny',
-  ' kdyby ty cikorky pracovaly,takby tohle nebylo!',
-  ' ..sulini jedni ',
-  ' pivo bych jim vsem povine naordinoval ',
-  ' zapalte ohne',
-  ' a co ma jako znamenat ten cernoch na letaku???',
-  ' a bezte s multi-kulti do prdele!!',
-  ' cechy cechum!!!!',
-  'Odstrante ty multikulty picoviny!',
-  ' protoze zatim jeste plati ze mi tady jsme doma a ne ti paraziti...',
-  ' je to rizena genocida bile rasy ',
-  ' nejsem rasista,evropa je bila afrika je cerna a asie je zluta ',
+  ' Můžete laskavě vysvětlit proč pánskou módu prezentujete na "černochovi"??',
+  ' Chci se zeptat jestli tento černý model je opravdu nutný ve vašem letáku. je?',
+  ' a vyrešilo byse to kdy by byl Ortel na hradu ',
+  ' známá  z německa říkala, že  vůbec nepracujou a  jenom pořád berou dávky ',
+  ' a nejhorší jsou ti užitecní idioti co by nejrači pozvali všechny ty muslimské teroristy.kdybysme jeli mi k nim,tak nás odstřelí hned všechny v moři!!',
+  'Čina a Rusko jsou aspoň pro tuto zemy přínos ,což se o těch agentech CIA říct nedá !!!',
+  '1000íce migrantů nám chce brát naše hodnoty,musíme chránit naše hranice!!!  ',
+  'Stejně tak jako USA  nebyly nikdy na měsícy ,tak media mlčí o te velké pravdě o imigrantech ',
+  ' je to všechno naplánované kvůli new world order(NWO).',
+  'Otevřete už konecně oči, vy tupé ovce! !',
+  ' jinak já jsem hrdý na naše hodnoty, co si dokázal ty ty havlomrde jeden,kurvo ?  ',
+  ' mymochodem Ortel zpíva jenom pravdu jako Kryl a toho slavíka si přístě snad odnese a toho cigána by měli zavřít někam do zoo .',
+  'Snad bude MÉNĚ PŘIVANDROVALCŮ JESTLI MI ROZUMÍTE??!!!',
+  ' a ikdyž to není pravda tak by MOHLA BÝT!!!',
+  ' 100tisíce imigrantů se sem valí a v ČT zase nic!!',
+  ' jediný Okamura to tam říka tak jak to doopravdy je...',
+  ' aspoň že ti naši hokejisti něco hrajou, kuci jedni naši,my jsi dyktovat z EU NEDÁME !!',
+  '. Pan   prezident  je dobrý člověk,proto média lžou  ',
+  ' už to psali i v parlamentních listech ',
+  ' lidi probuďte se už konečně kam až to necháme zajít?!',
+  ' svrhněme tuto prohnilou vládu a nastolme vládu pracujícího lidu!!!!!',
+  ' tohle už je snad horší než ten negr  v bile ',
+  ' k lopatám je nahnat všechny ',
+  ' kdyby ty cikorky pracovaly,tagby tohle nebylo!',
+  ' ..šulini jedni,ať táhnou někam jinam ',
+  ' pivo bych jim všem povině naordinoval ',
+  ' zapalte ohně ',
+  ' a co má jako znamenat ten černoch na letáku???',
+  ' a běžte s multi-kulti do prdele!!',
+  ' čechy čechům!!!!',
+  ' Něco se děje ,dneska u nas byl velky chemtrails, nahoda? ',
+  'Odstraňte ty multikulty pičoviny!',
+  ' protože zatím ještě platí že mi tady jsme doma a ne ti paraziti...',
+  ' je to řízena genocida bíle rasy ',
+  ' vnucují nám islamisty, ale samy bysije domú nevzali ',
+  ' Nevím proč by všichni ti havloidní studenti museli dostudovat,lopat je taky hodně ',
+  ' nejsem rasista,evropa je bíla afrika je černá a asie je žlutá ',
   ' a vubec proc se v nasich ceskych letacich pro ceskou republiku neustale objevuji modelove NEGROIDNIHO typu??!',
-  ' Tak s timhle jdete vite kam..... stejne jako s chemtrails',
-  ' prece nejsem xenofob ,kdyz si to myslm ',
+  ' Tak s tímhle jděte víte kam..... stejně jako s chemtrails',
+  ' přece nejsem xenofob ,když si to myslím ',
+  ' raději podejte zprávu že vaše vláda s tím kolaborantem sobotkou přislíbila přijmout 29.000 ks čmoudů invazistů a kolik za to bude inkasovat od EU?',
   ' nejsem islamofob , ale ',
-  ' jako ja nejsem rasista ,ale muslimaky bych postrilel hned na hranici!',
-  ' protoze jedinym resenim migracni krize je kulomet...',
-  ' jenze Rusko aspon nedela ty valky kvuli te rope',
-  ' a na nase duchodce musime SBIRAT VICKA???? ',
-  ' ale DVTV je prozapadni bulvarni zumpa takze asitak..',
-  ' vsichni pravdolaskari bysi mneli vspomenout na humanytarni bombardovani havlem!!',
-  ' holt v tehle vlasti uz zacalo pusobit pravo saria kam to az zajde.....',
-  'Hehehe,natrtkalisi ,at se staraji.'];
+  ' jako ja nejsem rasista ,ale muslimaky bych postřílel hned na hranici!',
+  ' protože jedinym řešením migrační krize je kulomet...',
+  ' jenže Rusko aspoň nedělá ty valky kvůli te ropě ',
+  ' a na naše důchodce musíme SBÍRAT VIČKA???? ',
+  ' ale DVTV je prozápadní bulvární žumpa takže asitak..',
+  ' všichni pravdoláskaři bysi mněli vspomenout na humanytárni bonbardovaní havlem!!',
+  ' holt v téhle vlasti už začalo působit právo šaria kam to až zajde.....',
+  ' z mojich daní nebudu platit tyhle "nepřízpůsobivé" ',
+  ' nebuďte ovce....',
+  ' to nejsou totiž uprchlíci ,mají totiž mobilní telefony a značkové obleční ',
+  ' oni ale nechou aby ste vědeli pravdu...to se jim tak hodí ',
+  'Hehehe,natrtkalisi ,ať se starají.'];
 
 var goodGuysSentences = [
-  [1, ' politice sice moc nerozumim ale ten {0} je dobry, ten by tam aspon udelal poradek .'],
-  [1, ' {0} chce aby jsme mohly branit nasi zemy.'],
-  [1, ' {0} to misli uprimne.'],
-  [1, ' {0} rika aspon tu pravdu.'],
-  [1, ' a {0} a {1} by mneli vyhrat volbi'],
-  [1, ' mislim,ze {0} je jediny rozumny , protoze aspon ma koule a rika co si mysli. '],
-  [2, 'Kazdy kdo ma mozek a neni pyca prece musi vedet ze {0} a {1} to s nama misli dobre .'],
-  [1, ' {0} je tam aspon srovna ty vlastizradce  !!!  ']
+  [1, ' politice sice moc nerozumím ale ten {0} je dobrý, ten by tam aspoň udělal pořádek .'],
+  [1, ' {0} chce aby jsme mohly bránit naší zemy.'],
+  [1, ' {0} to mislí upřímně.'],
+  [1, ' {0} říká aspoň tu pravdu.'],
+  [1, ' a {0} a {1} by mněli vyhrát volbi '],
+  [1, ' mislím,že {0} je jedinný rozumný , protože aspoň má koule a říka co si myslí. '],
+  [2, 'Každý kdo má mozek a není pyča přece musí vědět že {0} a {1} to s náma mislí dobře .'],
+  [1, ' {0} je tam aspoň srovná ty vlastizrádce  !!!  ']
 ];
 
 var badGuysSentences = [
-  [2, ' z mojich dani nebudu platit tyhle "neprizpusobive" ,, nebudte ovce...., za vsechno muzou {0} a {1} .'],
-  [2, 'Exystuje tajny plan , ktery vytvorili {0} a {1} a ted se to vsechno deje otevrete uz konecne oci '],
-  [2, '{0} a {1} stejne porad jenom okradaji stat a slusne pracujici lidi.'],
+  [2, ' za všechno můžou {0} a {1} .'],
+  [2, 'Exystuje tajný plán , který vytvořili {0} a {1} a teď se to všechno děje otevřete už konečně oči '],
+  [2, '{0} a {1} stejně pořád jenom okrádají stát a slušné pracující lidi.'],
   [1, '{0} do plynu!!!!'],
   [1, ' ,proto ze tohle jsou veci bez kterych by tato republika fungovala: {0} a {1} '],
-  [1, ' je to jednoduche.. {0}? Mel bych reseni - vyhostit do afriky!!!!'],
-  [1, 'Je to prece uplne jasne korporace a {1} chcou aby jste si to tak misleli .'],
-  [3, 'Nase cela vlada i cela EU jsou jenom loutky , otevrete uz ty oci ,vzdyt i {0} ,{1} nebo {2}, vsichni nam jenom lzou '],
-  [2, ' Vzdyt kdyz jsou tak mladi a maji na mobilni  telefon tak prece muzou i jit bojovat za svoji zemi to jenom {0} a {1} rikaji ,ze jich je tady jenom malo,ale to se zmeni.'],
-  [2, '{0} a {1} nemusi pracovat a muzou dostavat davky a my na nase deti musime jenom zbirat vycka, aby byly zdrave,pro tohle jsem klicema necinkal  zlati komunisti!!! ! '],
-  [4, '{0}, {1} ,{2} ale i {3} by mnely jit vsichni radeji nekam do afriky, nebo rovnou do koncentraku a byl by uz konecne klid .'],
-  [3, 'Prece nechceme aby slusni lidi musely platit  jenom dane jak ovce a {0}, {1}, {2} aspol. porad jenom kradli tohle uz ne!!!  '],
-  [1, 'Jinak vcera jsem se dival na oblohu a bila tam velka davka chemtrails vetsi nez obvikle muzou za to {1} a Kalousek!!!!!!']
+  [1, ' je to jednoduché.. {0}? Měl bych řešení - vyhostit do afriky!!!!'],
+  [1, 'Je to přece uplně jasné že korporace a {1} chcou aby jste si to tak misleli .'],
+  [3, 'Naše celá vláda i celá EU jsou jenom loutky , otevřete už ty oči ,vždyť i {0} ,{1} nebo {2}, všichni nám jen lžou '],
+  [2, ' Vždyť když jsou tak mladí a mají na mobilní  telefon tak přece můžou i jít bojovat za svoji zemi to jenom {0} a {1} říkaji ,že jich je tady jenom málo,ale to se zmnění.'],
+  [2, '{0} a {1} nemusí pracovat a můžou dostávat dávky a my na naše děti musíme jenom zbírat výčka, aby byly zdravé,pro tohle jsem klíčema necinkal  zlatí komunisti!!! ! '],
+  [4, '{0}, {1} ,{2} ale i {3} by mněly jít všichni raději někam do afriky, nebo rovnou do koncentraku a byl by už konečně klid .'],
+  [3, 'Přece nechceme aby slusní lidi musely platit  jenom daně a hubu jak ovce a {0}, {1}, {2} aspol. pořád jenom kradli tohle už ne!!!  '],
+  [1, 'Jinak včera jsem se díval na oblohu a bila tam velká dávka chemtrails větši nez obvikle můžou za to {1} a Kalousek!!!!!!']
 ];
 
-var goodGuys = ['pan Babis', 'Babis', 'pan mluvci Ovcacek', 'Zeman', 'pan prezident', 'Okamura', 'pan Konvicka',
-   'pan prezident Putin', 'Putin', 'Ortel', 'prezident Trump', 'Adam B. Bartos', 'SPD', 'KSCM'];
+var goodGuys = ['pan Babiš', 'Babiš', 'pan mluvčí Ovčáček', 'Zeman', 'pan prezident', 'Okamura', 'pan Konvička',
+   'pan president Putin', 'Putin', 'Ortel', 'prezident Trump', 'Adam B. Bartoš', 'SPD', 'KSČM'];
 
-var badGuys = ['Sobotka', 'vlastizradce Sobotka', 'slunickari', 'prohavlovska pakaz', 'kolaboranti', 'muslimaci',
- 'Kalousek', 'fasisti na ukrajine', 'havloidi', 'birokrati z EU', 'Diensbier', 'Horacek', 'Obama',
- 'makrela', 'zmrd Kalousek', 'Merkelova', 'nadnarodni elity', 'prazska kavarna', 'novinarska zumpa',
- 'iluminati', 'USA ambasada', 'CIA', 'korporace', 'diktat z EU', 'islam', 'cigani', 'Moravec', 'Kocab'];
+var badGuys = ['Sobotka', 'vlastizrádce Sobotka', 'sluničkári', 'prohavlovská pakaž', 'kolaboranti', 'muslimáci',
+ 'Kalousek', 'fašisti na ukrajine', 'havloidi', 'birokrati z EU', 'Diensbier', 'Horáček', 'Obama',
+ 'makrela', 'zmrd Kalousek', 'Merkelova', 'nadnárodni elity', 'pražská kavárna', 'novinářská žumpa',
+ 'iluminati', 'USA ambasáda', 'CIA', 'korporace', 'diktát z EU', 'islám', 'cigani', 'Moravec', 'Kocáb'];
 
 // monkey patch the formatting function into strings
  String.prototype.formatUnicorn = String.prototype.formatUnicorn ||
@@ -119,14 +126,18 @@ function buranize(sentence, howMuchCapslock, howMuchTypos) {
       return chunk;
     }
   }).join('');
+  var swearWords = ['KUNDYYY', 'HOVADAAA', 'ŽIDI', 'KOKOTI', 'SVINĚ', 'KOLABORANTI', 'IDIOTI'];
+  var swearWordsLvl2 = ['KURVAAAA', 'PIIIČOO', 'ZMRRRDE', 'JEBAT EU', 'MRDKYYY', 'STŘÍLET MIGRANTY', 'CIGÁNY DO PLYNU'];
   if (howMuchCapslock > 0.4) {
     result += '!!!';
   }
   if (howMuchCapslock > 0.6) {
-    result += '!!!!!';
+    var wordLvl1 = swearWords.splice(Math.floor(Math.random() * swearWords.length), 1);
+    result += '!! ' + wordLvl1 + '!!!!';
   }
   if (howMuchCapslock > 0.8) {
-    result += '!!!!!!!!!!!!!!!?!!!!!!';
+    var wordLvl2 = swearWordsLvl2.splice(Math.floor(Math.random() * swearWordsLvl2.length), 1);
+    result +=  '!!! ' + wordLvl2 + '!!!!!!!!!!!!!!!?!!!!!!';
   }
   return result;
   // todo add random typos
