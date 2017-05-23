ca23ce0be885ddecef035a6ede665bd6d1e38f83
Jirka Kremser <jkremser@redhat.com>
2017-05-24 01:21:08 +0200

    more hates (+kajinek included)

--- generator.js
+++ generator.js
@@ -7,6 +7,7 @@ var other = ['Přece  nám nebudou říkat jestli máme mít právo šaria , piv
   ' a média a česká televize mlčí',
   ' ja nejsem rasista..',
   ' Můžete laskavě vysvětlit proč pánskou módu prezentujete na "černochovi"??',
+  // https://cloudia.hnonline.sk/r750xn/69736144bf4883c2918fde44d428cdc0.jpg
   ' Chci se zeptat jestli tento černý model je opravdu nutný ve vašem letáku. je?',
   ' a vyrešilo byse to kdy by byl Ortel na hradu ',
   ' známá  z německa říkala, že  vůbec nepracujou a  jenom pořád berou dávky ',
@@ -15,10 +16,10 @@ var other = ['Přece  nám nebudou říkat jestli máme mít právo šaria , piv
   '1000íce migrantů nám chce brát naše hodnoty,musíme chránit naše hranice!!!  ',
   'Stejně tak jako USA  nebyly nikdy na měsícy ,tak media mlčí o te velké pravdě o imigrantech ',
   ' je to všechno naplánované kvůli new world order(NWO).',
-  'Otevřete už konecně oči, vy tupé ovce! !',
+  ' Otevřete už konecně oči, vy tupé ovce! !',
   ' jinak já jsem hrdý na naše hodnoty, co si dokázal ty ty havlomrde jeden,kurvo ?  ',
   ' mymochodem Ortel zpíva jenom pravdu jako Kryl a toho slavíka si přístě snad odnese a toho cigána by měli zavřít někam do zoo .',
-  'Snad bude MÉNĚ PŘIVANDROVALCŮ JESTLI MI ROZUMÍTE??!!!',
+  ' Snad bude MÉNĚ PŘIVANDROVALCŮ JESTLI MI ROZUMÍTE??!!!',
   ' a ikdyž to není pravda tak by MOHLA BÝT!!!',
   ' 100tisíce imigrantů se sem valí a v ČT zase nic!!',
   ' jediný Okamura to tam říka tak jak to doopravdy je...',
@@ -31,7 +32,6 @@ var other = ['Přece  nám nebudou říkat jestli máme mít právo šaria , piv
   ' k lopatám je nahnat všechny ',
   ' kdyby ty cikorky pracovaly,tagby tohle nebylo!',
   ' ..šulini jedni,ať táhnou někam jinam ',
-  ' pivo bych jim všem povině naordinoval ',
   ' zapalte ohně ',
   ' a co má jako znamenat ten černoch na letáku???',
   ' a běžte s multi-kulti do prdele!!',
@@ -39,10 +39,12 @@ var other = ['Přece  nám nebudou říkat jestli máme mít právo šaria , piv
   ' Něco se děje ,dneska u nas byl velky chemtrails, nahoda? ',
   'Odstraňte ty multikulty pičoviny!',
   ' protože zatím ještě platí že mi tady jsme doma a ne ti paraziti...',
+  // https://cloudia.hnonline.sk/r750xn/69736144bf4883c2918fde44d428cdc0.jpg
   ' je to řízena genocida bíle rasy ',
   ' vnucují nám islamisty, ale samy bysije domú nevzali ',
   ' Nevím proč by všichni ti havloidní studenti museli dostudovat,lopat je taky hodně ',
   ' nejsem rasista,evropa je bíla afrika je černá a asie je žlutá ',
+  // https://cloudia.hnonline.sk/r750xn/69736144bf4883c2918fde44d428cdc0.jpg
   ' a vubec proc se v nasich ceskych letacich pro ceskou republiku neustale objevuji modelove NEGROIDNIHO typu??!',
   ' Tak s tímhle jděte víte kam..... stejně jako s chemtrails',
   ' přece nejsem xenofob ,když si to myslím ',
@@ -59,42 +61,98 @@ var other = ['Přece  nám nebudou říkat jestli máme mít právo šaria , piv
   ' nebuďte ovce....',
   ' to nejsou totiž uprchlíci ,mají totiž mobilní telefony a značkové obleční ',
   ' oni ale nechou aby ste vědeli pravdu...to se jim tak hodí ',
-  'Hehehe,natrtkalisi ,ať se starají.'];
+  // http://zpravy.idnes.cz/foto.aspx?r=domaci&c=A161129_110614_domaci_fer
+  ' Tvl Bango......to byl ČESKÝ Slavík....tak co se tam máš co srát Ty náplavo z indije.',
+  // http://zpravy.idnes.cz/foto.aspx?r=domaci&c=A161129_110614_domaci_fer
+  ' Banga je cigoš a bojí se plynu jako všechny ty mrdky přivandrovaný.',
+  // http://zpravy.idnes.cz/diskuse.aspx?iddiskuse=A161129_110614_domaci_fer&razeni=vlakno&strana=9
+  ' To je tak když se většina přizpůsobuje menšině.A menšina si myslí že může všechno.',
+  ' Islám v ČR nechceme!! ',
+  ' A jako vítězové voleb vás, milí muslimové, nameleme do masokostní moučky.',
+  // diskuterka na FB profilu od M. Konvicky:
+  ' Tahle fotka.......zvracení, jen zvracení vyvolává....',
+  // https://www.novinky.cz/diskuse?id=486596&articleId=/domaci/438673-prvni-rozhovor-s-kajinkem-citim-se-svobodny-pujdu-na-karluv-most.html&sectionId=8
+  '..........mělo by se objasnit, jak to vlastně všechno bylo.....',
+  ' Hehehe,natrtkalisi ,ať se starají.'];
 
 var goodGuysSentences = [
-  [1, ' politice sice moc nerozumím ale ten {0} je dobrý, ten by tam aspoň udělal pořádek .'],
-  [1, ' {0} chce aby jsme mohly bránit naší zemy.'],
-  [1, ' {0} to mislí upřímně.'],
-  [1, ' {0} říká aspoň tu pravdu.'],
-  [1, ' a {0} a {1} by mněli vyhrát volbi '],
-  [1, ' mislím,že {0} je jedinný rozumný , protože aspoň má koule a říka co si myslí. '],
-  [2, 'Každý kdo má mozek a není pyča přece musí vědět že {0} a {1} to s náma mislí dobře .'],
-  [1, ' {0} je tam aspoň srovná ty vlastizrádce  !!!  ']
+  ' politice sice moc nerozumím ale ten {0} je dobrý, ten by tam aspoň udělal pořádek .',
+  ' {0} chce aby jsme mohly bránit naší zemy.',
+  ' {0} to mislí upřímně.',
+  ' {0} říká aspoň tu pravdu.',
+  ' a {0} a {1} by mněli vyhrát volbi ',
+  ' mislím,že {0} je jedinný rozumný , protože aspoň má koule a říka co si myslí. ',
+  ' Každý kdo má mozek a není pyča přece musí vědět že {0} a {1} to s náma mislí dobře .',
+  ' {0} je tam aspoň srovná ty vlastizrádce  !!!  '
 ];
 
-var badGuysSentences = [
-  [2, ' za všechno můžou {0} a {1} .'],
-  [2, 'Exystuje tajný plán , který vytvořili {0} a {1} a teď se to všechno děje otevřete už konečně oči '],
-  [2, '{0} a {1} stejně pořád jenom okrádají stát a slušné pracující lidi.'],
-  [1, '{0} do plynu!!!!'],
-  [1, ' ,proto ze tohle jsou veci bez kterych by tato republika fungovala: {0} a {1} '],
-  [1, ' je to jednoduché.. {0}? Měl bych řešení - vyhostit do afriky!!!!'],
-  [1, 'Je to přece uplně jasné že korporace a {1} chcou aby jste si to tak misleli .'],
-  [3, 'Naše celá vláda i celá EU jsou jenom loutky , otevřete už ty oči ,vždyť i {0} ,{1} nebo {2}, všichni nám jen lžou '],
-  [2, ' Vždyť když jsou tak mladí a mají na mobilní  telefon tak přece můžou i jít bojovat za svoji zemi to jenom {0} a {1} říkaji ,že jich je tady jenom málo,ale to se zmnění.'],
-  [2, '{0} a {1} nemusí pracovat a můžou dostávat dávky a my na naše děti musíme jenom zbírat výčka, aby byly zdravé,pro tohle jsem klíčema necinkal  zlatí komunisti!!! ! '],
-  [4, '{0}, {1} ,{2} ale i {3} by mněly jít všichni raději někam do afriky, nebo rovnou do koncentraku a byl by už konečně klid .'],
-  [3, 'Přece nechceme aby slusní lidi musely platit  jenom daně a hubu jak ovce a {0}, {1}, {2} aspol. pořád jenom kradli tohle už ne!!!  '],
-  [1, 'Jinak včera jsem se díval na oblohu a bila tam velká dávka chemtrails větši nez obvikle můžou za to {1} a Kalousek!!!!!!']
+// male only
+var badGuysSentencesSingular = [
+  ' Ten {0} už fakt neví jak by na sebe upozornil ,ten terorystický útok v Menčestru je určitě jeho práce, říkal mi to známý, co dělá na policii!V televizi zase nic!',
+  // https://www.novinky.cz/diskuse?id=486570&articleId=/domaci/438651-sprosty-vrah-slusnemu-cloveku-je-nevolno-reagoval-kalousek-na-milost-pro-kajinka.html&sectionId=8&page=2
+  ' Jak múže mít tahle žumpa pomluv a nadávak {0} takový prostor k šíření svých bludů ? Kdo ho ještě podporuje ?Už aby byly volby !',
+  ' On ten {0} ma asi v hlave uplne nasr*ano neboco!',
+  ' {0} by asi potřeboval abyho ti negři imigrantský vzali pěkně zezadu abyje přestal hájit.....'
+];
+
+var badGuysSentencesPlural = [
+  ' za všechno můžou {0} a {1} .',
+  'Exystuje tajný plán , který vytvořili {0} a {1} a teď se to všechno děje otevřete už konečně oči ',
+  '{0} a {1} stejně pořád jenom okrádají stát a slušné pracující lidi.',
+  '{0} do plynu!!!!',
+  ' ,proto ze tohle jsou veci bez kterych by tato republika fungovala: {0} a {1} ',
+  ' je to jednoduché.. {0}? Měl bych řešení - vyhostit do afriky!!!!',
+  'Je to přece uplně jasné že korporace a {1} chcou aby jste si to tak misleli .',
+  'Naše celá vláda i celá EU jsou jenom loutky , otevřete už ty oči ,vždyť i {0} ,{1} nebo {2}, všichni nám jen lžou ',
+  ' Vždyť když jsou tak mladí a mají na mobilní  telefon tak přece můžou i jít bojovat za svoji zemi to jenom {0} a {1} říkaji ,že jich je tady jenom málo,ale to se zmnění.',
+  '{0} a {1} nemusí pracovat a můžou dostávat dávky a my na naše děti musíme jenom zbírat výčka, aby byly zdravé,pro tohle jsem klíčema necinkal  zlatí komunisti!!! ! ',
+  '{0}, {1} ,{2} ale i {3} by mněly jít všichni raději někam do afriky, nebo rovnou do koncentraku a byl by už konečně klid .',
+  'Přece nechceme aby slušní lidi musely platit  jenom daně a hubu jak ovce a {0}, {1}, {2} aspol. pořád jenom kradli tohle už ne!!!  ',
+  'Jinak včera jsem se díval na oblohu a bila tam velká dávka chemtrails větši nez obvikle můžou za to {1} a Kalousek!!!!!!'
 ];
 
 var goodGuys = ['pan Babiš', 'Babiš', 'pan mluvčí Ovčáček', 'Zeman', 'pan prezident', 'Okamura', 'pan Konvička',
    'pan president Putin', 'Putin', 'Ortel', 'prezident Trump', 'Adam B. Bartoš', 'SPD', 'KSČM'];
 
-var badGuys = ['Sobotka', 'vlastizrádce Sobotka', 'sluničkári', 'prohavlovská pakaž', 'kolaboranti', 'muslimáci',
- 'Kalousek', 'fašisti na ukrajine', 'havloidi', 'birokrati z EU', 'Diensbier', 'Horáček', 'Obama',
+var badGuysGuys = ['Sobotka', 'vlastizrádce Sobotka', 'Kalous', 'Diensbier', 'Horáček', 'Obama', 'zmrd Kalousek',
+   'Moravec', 'Kocáb'];
+
+var otherBadGuys = ['Sobotka', 'vlastizrádce Sobotka', 'sluničkári', 'prohavlovská pakaž', 'kolaboranti', 'muslimáci',
+ 'Kalousek', 'fašisti na ukrajine', 'havloidi', 'birokrati z EU', 'Diensbier', 'Horáček', 'Obama', 'eurofanatici',
  'makrela', 'zmrd Kalousek', 'Merkelova', 'nadnárodni elity', 'pražská kavárna', 'novinářská žumpa',
  'iluminati', 'USA ambasáda', 'CIA', 'korporace', 'diktát z EU', 'islám', 'cigani', 'Moravec', 'Kocáb'];
+
+var otherThingsWorthHating = ['integrací', 'eurofederalizací', 'diskriminací', 'emancipací', 'eurofašizací', 'inkluzí', 'globalizací'];
+var otherThingsWorthHatingSentences = [
+  ' ať jdou s tou {0} už do PRDELE!',
+  ' s {0} ať si jdou demonstrovat do Bruselu......',
+  ' jako nic proti lidským právům, ale s tou {0} to je slepá ulička..',
+  ' {0} posedlí sluníčkáři,nic víc....',
+  ' Neziskovky tu zaplavují médija s {0} a mezitím deseti1000íce přivandrovalců se sem mlčky hrne!!'
+];
+
+var randomBullshit = [
+  // https://www.novinky.cz/diskuse?id=486596&articleId=/domaci/438673-prvni-rozhovor-s-kajinkem-citim-se-svobodny-pujdu-na-karluv-most.html&sectionId=8
+  ' JÁ TU STÁLE PSAL , PROPUSTIT A ŘÁDNĚ ODŠKODNIT, NIC NEUDĚLAL A MNOHO ODBORNÍKŮ TADY SE DO MĚ POUŠTĚLO , NEUDĚL A PRÁVEM JE NA SVOBODĚ , KDO ZNÁ NAŠE SOUDY , TAK TO POCHOPÍ !!!!!!!!',
+  // https://www.novinky.cz/diskuse?id=486570&articleId=/domaci/438651-sprosty-vrah-slusnemu-cloveku-je-nevolno-reagoval-kalousek-na-milost-pro-kajinka.html&sectionId=8&page=3
+  ' Zle je mi především z Kalouska,opilce a žvanila, kterého nenávidí 99% české populace a který denodenně otravuje a znechucuje posluchače hlabvní zpravodaj. relace ČT!',
+  // https://www.novinky.cz/diskuse?id=486562&articleId=/domaci/438643-uz-mam-pracovni-nabidky-ale-ted-se-chci-rozkoukat-rekl-kajinek-za-branou-veznice.html&sectionId=8
+  ' Má pěkný tričko, zelená je barva jara, síly a naděje ! Tak snad už bude ten nahoře stát při něm.Věřím mu, že to neudělal a Zemanovi díky za odvahu, že mu dal milost "proti všem".',
+  // https://www.novinky.cz/diskuse?id=486429&articleId=/domaci/438539-sobotka-dava-zemanovi-s-odvolanim-babise-ultimatum-do-patku.html&sectionId=8
+  ' Co si to ten zaprodaný ploskolebec dovoluje? On je ten, který měl být dávno pryč z politiky! Ví, že po volbách půjde, tak ještě chce honem nahrabat za 60000 migrantů! To mu přece nemůže projít!',
+  // http://zpravy.idnes.cz/foto.aspx?r=domaci&c=A161129_110614_domaci_fer
+  ' Bango zbal sebe a celou rodinu a romskou komunytu a jdete do kanady stát aspon ušetří nekolik miliard korun když je to tady stát plnej nácku.Pochop to už že lidi jsou nasraní že musí vydělávat na sociálky nepracujících které jsou mnohdy vyšší než jejich platy .JO A UPRCHLIKY SI VEM TAKY CHUDINKO.',
+  // http://zpravy.idnes.cz/foto.aspx?r=domaci&c=A161129_110614_domaci_fer
+  ' Seš jet tupej cikán Bango....A nic víc!!!Sere tě ze se český národ spojil proti dementum jako jsi ty a podobní...běž zase krást kola a vykrádat sklepy cikáne... :-) :-) :-)',
+  // diskutujici na https://www.facebook.com/KonvaMartin/
+  ' V pripade Davida Cerneho ve skutecnosti se o zadneho umelce nejedna. Je to obycejna moralni zruda, parazit a vyzirka penez danovych poplatniku. Typicky slunickarsky zlocinec...',
+  //https://www.novinky.cz/diskuse?id=486596&articleId=/domaci/438673-prvni-rozhovor-s-kajinkem-citim-se-svobodny-pujdu-na-karluv-most.html&sectionId=8
+  ' Jirko,přeju Ti klidný život,bez dotěrných lidí a novinářů.. ❤ Ty pro mě NIKDY nebudeš vrahem!Vrahem pro mě vždy bude ten,,druhý!!kvůli kterému tě justice nespravedlivě okradla o 23 let života :-* ',
+  //https://www.novinky.cz/diskuse?id=486596&articleId=/domaci/438673-prvni-rozhovor-s-kajinkem-citim-se-svobodny-pujdu-na-karluv-most.html&sectionId=8
+  ' Pane Kájínek pravda se ukázala trvale opálený bez mozku nemá co svědčit ale veledůložitá věc je že jste uvolnil zásluhou pana prezidenta místo v base panu premierovy sobotkovi. Díky.',
+  //https://www.novinky.cz/diskuse?id=486596&articleId=/domaci/438673-prvni-rozhovor-s-kajinkem-citim-se-svobodny-pujdu-na-karluv-most.html&sectionId=8
+  ' Vítej na svobodě Jirko a dej na sebe pozor .Mám velké obavy ,že Tě demokrati připraví o život a nebo dostanou zpátky do krymu nějakou vykonstruovanou provokací. '
+];
 
 // monkey patch the formatting function into strings
  String.prototype.formatUnicorn = String.prototype.formatUnicorn ||
@@ -137,7 +195,7 @@ function buranize(sentence, howMuchCapslock, howMuchTypos) {
   }
   if (howMuchCapslock > 0.8) {
     var wordLvl2 = swearWordsLvl2.splice(Math.floor(Math.random() * swearWordsLvl2.length), 1);
-    result +=  '!!! ' + wordLvl2 + '!!!!!!!!!!!!!!!?!!!!!!';
+    result += '!!! ' + wordLvl2 + '!!!!!!!!!!!!!!!?!!!!!!';
   }
   return result;
   // todo add random typos
@@ -162,22 +220,38 @@ function shuffle(array) {
   return array;
 }
 
+function pickRandomElement(array) {
+  return array.splice(Math.floor(Math.random() * array.length), 1)[0];
+}
+
+function makeSentence(templates, subjects) {
+  return pickRandomElement(templates).formatUnicorn(shuffle(subjects));
+}
+
+var GOOD_GUYS_SENTENCE_PROB = 0.09;
+var BAD_GUYS_SENTENCE_PROB = 0.14;
+var OTHER_THINGS_WORTH_HATING_PROB = 0.04;
+var WHOLE_RANDOM_POST_PROB = 0.05;
+
 function getRandomSentence() {
   var r = Math.random();
-  if (r < 0.12) {
+
+  // kolo stesti
+  if (r < GOOD_GUYS_SENTENCE_PROB) {
     //goodGuysSentences
-    var item = goodGuysSentences.splice(Math.floor(Math.random() * goodGuysSentences.length), 1);
-    // var numberOfPeople = item[0][0];
-    var sentenceTemplate = item[0][1];
-    return sentenceTemplate.formatUnicorn(shuffle(goodGuys));
-  } else if (r < 0.25) {
+    return makeSentence(goodGuysSentences, goodGuys);
+  } else if (r < GOOD_GUYS_SENTENCE_PROB + BAD_GUYS_SENTENCE_PROB) {
     //badGuysSentences
-    var item = badGuysSentences.splice(Math.floor(Math.random() * badGuysSentences.length), 1);
-    // var numberOfPeople = item[0][0];
-    var sentenceTemplate = item[0][1];
-    return sentenceTemplate.formatUnicorn(shuffle(badGuys));
+    if (Math.random() < 0.33) { // singular
+      return makeSentence(badGuysSentencesSingular, badGuysGuys);
+    } else { // plular
+      return makeSentence(badGuysSentencesPlural, otherBadGuys);
+    }
+  } else if (r < GOOD_GUYS_SENTENCE_PROB + BAD_GUYS_SENTENCE_PROB + OTHER_THINGS_WORTH_HATING_PROB) {
+    // otherThingsWorthHating
+    return makeSentence(otherThingsWorthHatingSentences, otherThingsWorthHating);
   } else {
-    //other
+    //other (generic sentences)
     return other.splice(Math.floor(Math.random() * other.length), 1);
   }
 }
@@ -187,17 +261,28 @@ function getPost(length, capslock) {
   length = length || Math.random() * 3 + 2;
   capslock = capslock || 0.05;
   var goodGuysSentencesBak = goodGuysSentences.slice(0);
-  var badGuysSentencesBak = badGuysSentences.slice(0);
+  var badGuysSentencesSingularBak = badGuysSentencesSingular.slice(0);
+  var badGuysSentencesPluralBak = badGuysSentencesPlural.slice(0);
   var otherBak = other.slice(0);
+  var randomBullshitBak = randomBullshit.slice(0);
+  var otherThingsWorthHatingSentencesBak = otherThingsWorthHatingSentences.slice(0);
 
   var post = '';
-  for (var i = 0; i < length; i++) {
-    post += getRandomSentence();
+  if (Math.random() < WHOLE_RANDOM_POST_PROB) {
+    post = pickRandomElement(randomBullshit);
+  } else {
+    for (var i = 0; i < length; i++) {
+      post += getRandomSentence();
+    }
   }
 
   goodGuysSentences = goodGuysSentencesBak;
-  badGuysSentences = badGuysSentencesBak;
+  badGuysSentencesSingular = badGuysSentencesSingularBak;
+  badGuysSentencesPlural = badGuysSentencesPluralBak;
   other = otherBak;
+  randomBullshit = randomBullshitBak;
+  otherThingsWorthHatingSentences = otherThingsWorthHatingSentencesBak;
+
   return buranize(post, capslock);
 }
 
@@ -210,6 +295,7 @@ function generatePost() {
 
 // hacky export, but let's keep it simple
 window.generatePost = generatePost;
+window.otherThingsWorthHatingSentences = otherThingsWorthHatingSentences;
 
 return {
   getPost: getPost
