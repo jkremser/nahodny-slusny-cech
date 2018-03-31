f7b6ceaff83804fe072e9f158e95a4a1b3cf4105
Jirka Kremser <jkremser@redhat.com>
2018-04-01 00:29:55 +0200

    changing selection background dynamically

--- js/generator.js
+++ js/generator.js
@@ -309,6 +309,10 @@ function changeBackground () {
   } else {
     elem.className = 'myButton';
   }
+  document.styleSheets[0].deleteRule(0);
+  document.styleSheets[0].deleteRule(0);
+  document.styleSheets[0].insertRule('::selection { background: ' + color + '4; }')
+  document.styleSheets[0].insertRule('::-moz-selection { background-color: ' + color + '4; }')
   document.getElementById('rageFactor').style.background = color;
 }
 
